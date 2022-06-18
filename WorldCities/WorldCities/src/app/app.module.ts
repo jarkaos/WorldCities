66a1697e548937841168dd1e7c58d4b18db391c9
jarkaos <americo.perez@icloud.com>
2022-06-18 19:50:07 +0200

    Sort,Filter,Paging for Cities

--- WorldCities/WorldCities/src/app/app.module.ts
+++ WorldCities/WorldCities/src/app/app.module.ts
@@ -6,21 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './modules/app-routing.module';
+import { CitiesComponent } from './components/cities/cities.component';
+import { AngularMaterialModule } from './modules/angular-material.module';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavMenuComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    CitiesComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
+    AngularMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- WorldCities/WorldCities/src/app/modules/app-routing.module.ts
+++ WorldCities/WorldCities/src/app/modules/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CitiesComponent } from '../components/cities/cities.component';
 import { HomeComponent } from '../components/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'cities', component: CitiesComponent },
 ];
 
 @NgModule({
